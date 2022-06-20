049ae734a8a4c5ce801b9871bdec98ee25c7f28d
progFoxx <nikitahanov1@gmail.com>
2022-06-20 10:05:44 +0500

    version has been been WIP

---
+++ PyQT_app.py
@@ -0,0 +1,6 @@
+run = True
+xiiiii = 5
+while run == True:
+    print(xiiiii)
+    if xiiiii < 5 or xiiiii > 5 :
+        run = False
---
+++ Sql_Work.py
@@ -0,0 +1,3 @@
+from CGI import *
+from PyQT_app import *
+datal=[500,999,333,555]
